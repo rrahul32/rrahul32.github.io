63b539b6133f4dba7717c4de14defb5799141a4c
Rahul Reghu <87755997+rrahul32@users.noreply.github.com>
2023-01-10 22:41:37 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,35 @@
+<script type="module">
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBUVT3B-N0O9GroCfFxGI7vZw_gcaOBM0E",
+
+    authDomain: "carrentalportal-e1005.firebaseapp.com",
+
+    projectId: "carrentalportal-e1005",
+
+    storageBucket: "carrentalportal-e1005.appspot.com",
+
+    messagingSenderId: "903510600582",
+
+    appId: "1:903510600582:web:3431aad7edd46bc340da07"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+</script>
